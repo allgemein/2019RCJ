1e3721ef5bbc178de103b79e654c2d9d6a090a48
zaba-pipipi <57747436+zaba-pipipi@users.noreply.github.com>
2019-11-18 23:11:07 +0900

    書き終わったやつ

--- front_obj.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "pin.h"
-#include "ultraSonicSensor.h"
-#include "linetrace.h"
-#include "right_angle_turn_Right.h"
-///正面に障害物が来たら止まるプログラム
-ultraSonicSensor usF(echoF,trigerF); //前の値を返すコマンドを指定する
-
-void find_front_obj() ;
