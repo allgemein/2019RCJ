cfa0e479f73e58eb68f4ad90d63537a08a667012
allgemein <amanetamada@gmail.com>
2019-11-19 17:40:13 +0900

    元に戻りました(n回目)

--- set_right_angle.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "pin.h"
-#include "ultraSonicSensor.h"
-#include "linetrace.h"
-#include "DCMotorControl.h"
-#include "turn_left.h"
-
-void set_right_angle() ;
--- set_right_angle.ino
+++
@@ -1,6 +0,0 @@
-#include"set_right_angle.h"
-///黒いラインに対して垂直に戻すプログラム
-
-void right_angle(){
-    
-}
